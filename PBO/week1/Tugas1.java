b704fc90fed24219a23fa9aa0f3befc11fbc0d74
Chalida Abdat <chalidabdat@gmail.com>
2025-03-02 15:33:39 +0700

    Add files via upload

---
+++ PBO/week1/Tugas1.java
@@ -0,0 +1,22 @@
+package week1;
+
+public class Tugas1 {
+    public static void main(String[] args){
+        String NIM = "A11.2023.15031";
+        String Nama = "Chalida Abdat";
+        String Alamat = "Jl. Ketileng";
+        String Kota = "Semarang";
+        String KodePos = "50272";
+        String NoHP = "085600547876";
+        String Email = "chalidabdat@gmail.com";
+
+        System.out.println("\n======= Tugas 1 Biodata =======");
+        System.out.println("NIM      : " + NIM);
+        System.out.println("Nama     : " + Nama);
+        System.out.println("Alamat   : " + Alamat);
+        System.out.println("Kota     : " + Kota);
+        System.out.println("Kode Pos : " + KodePos);
+        System.out.println("No HP    : " + NoHP);
+        System.out.println("Email    : " + Email);
+    }
+}
